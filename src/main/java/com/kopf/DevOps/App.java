2379bd78d4b88f838fed0fe630bff68fb4f6b28a
gkopf-PC\gkopf <george.kopf@gmail.com>
2017-02-08 20:15:58 -0500

    Second commit test.

--- src/main/java/com/kopf/DevOps/App.java
+++ src/main/java/com/kopf/DevOps/App.java
@@ -11,5 +11,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("This is the time.");
+        System.out.println("This is the record of the time.");
     }
 }
