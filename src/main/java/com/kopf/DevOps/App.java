3ddc2a44658b1fdf75988d87d95c0d1d7a65d465
gkopf-PC\gkopf <george.kopf@gmail.com>
2017-02-08 20:01:53 -0500

    Testing the commit process.

--- src/main/java/com/kopf/DevOps/App.java
+++ src/main/java/com/kopf/DevOps/App.java
@@ -7,7 +7,9 @@ package com.kopf.DevOps;
 public class App 
 {
     public static void main( String[] args )
+
     {
         System.out.println( "Hello World!" );
+        System.out.println("This is the time.");
     }
 }
